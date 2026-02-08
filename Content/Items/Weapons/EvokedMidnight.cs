606f72648b46cc9dcb18d315222ebefd5931b4fc
Rory Ochoa <thenyancatxd@gmail.com>
2026-02-08 13:33:35 -0800

    Update EvokedMidnight.cs

--- Content/Items/Weapons/EvokedMidnight.cs
+++ Content/Items/Weapons/EvokedMidnight.cs
@@ -17,7 +17,7 @@ namespace RoryMod.Content.Items.Weapons
 			Item.DefaultToStaff(ModContent.ProjectileType<DarkTear>(), 13, 25, 8);
 			//Item.DefaultToStaff(ProjectileID.BloodShot, 16, 25, 12);
 			Item.staff[Type] = true; // This makes the useStyle animate as a staff instead of as a gun.
-			Item.UseSound = SoundID.Item71;
+			Item.UseSound = SoundID.Item43;
 
 			// A special method that sets the damage, knockback, and bonus critical strike chance.
 			// This weapon has a crit of 32% which is added to the players default crit chance of 4%
